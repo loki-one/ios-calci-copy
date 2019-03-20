eecbaf05d38ca97a777f687b4282320c398dbdc5
Jadhav <lokesh.jadhav@sap.com>
2019-03-20 15:01:26 +0100

    :sparkles: Initial Commit

---
+++ calculator.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function getDisplayValue() {
+    const calciDisplayDiv = document.querySelector(".calci-display");
+    const displayValue = calciDisplayDiv.innerText;
+    return displayValue;
+}
+
+
+
+
+// let clickedButtonValue;
+// const calciKeypad = document.querySelector(".calci-keypad");
+// const calciDisplayDiv = document.querySelector(".calci-display");
+// let displayResult = "";
+// calciKeypad.addEventListener('click', function (event) {
+//     clickedButtonValue = event.target.innerText;
+//     if (clickedButtonValue != "C" && clickedButtonValue != "BS" && clickedButtonValue != "/" && clickedButtonValue != "*" && clickedButtonValue != "+" && clickedButtonValue != "-" && clickedButtonValue != "=") {
+//         displayResult += clickedButtonValue;
+//         calciDisplayDiv.innerText = displayResult;
+//     } else if (clickedButtonValue === "C") {
+//         displayResult = "";
+//         calciDisplayDiv.innerText = 0;
+//     } else if (clickedButtonValue === "/" || clickedButtonValue === "*" || clickedButtonValue === "+" || clickedButtonValue === "-") {
+
+//     }
+
+// });
+
+
+
+
