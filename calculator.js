09587c7f1964b10eb8f3c65978c4647dd2c5f524
Jadhav <lokesh.jadhav10@gmail.com>
2019-03-21 10:55:25 +0100

    new logic for calculation and display

--- calculator.js
+++ calculator.js
@@ -7,42 +7,51 @@
 //     return displayValue;
 // }
 
+function updateCalciDisplay(outputValue) {
+    const calciDisplayDiv = document.querySelector(".calci-display");
+    calciDisplayDiv.innerText = outputValue;
+}
+
 let inputValue;
+let calciMemory = [];
 const calciKeypad = document.querySelector(".calci-keypad");
-const calciDisplayDiv = document.querySelector(".calci-display");
-let inputNumbers = [];
-let operators = [];
-let displayResult = "";
+let valueInCalciDisplay = "";
 calciKeypad.addEventListener('click', function (event) {
     inputValue = event.target.innerText;
-    // if (inputValue != "C" && inputValue != "BS" && inputValue != "/" && inputValue != "*" && inputValue != "+" && inputValue != "-" && inputValue != "=") {
-    //     displayResult += inputValue;
-    //     calciDisplayDiv.innerText = displayResult;
-    // } else if (inputValue === "C") {
-    //     displayResult = "";
-    //     calciDisplayDiv.innerText = 0;
-    // } else if (inputValue === "/" || inputValue === "*" || inputValue === "+" || inputValue === "-") {
-    //     inputNumbers.push(parseInt(displayResult));
-    //     operators.push(inputValue);
-    //     console.log(inputNumbers);
-    //     console.log(operators);
-    //     displayResult = "";
-    // } else if (inputValue === "=") {
-    //     inputNumbers.push(parseInt(calciDisplayDiv.innerText));
-    //     let num1 = inputNumbers[0];
-    //     let num2 = inputNumbers[1];
-    //     let op = operators[0]
-    //     let calResult;
-    //     switch (op){
-    //         case '+':
-    //             calResult = num1 + num2;
-    //             break;
-    //         default:
-    //             console.log("Invalid expression");
-    //     }
-    //     calciDisplayDiv.innerText = calResult;
-    //     displayResult = "";
-    // }
+
+    if (inputValue === "C") {
+        valueInCalciDisplay = 0;
+        updateCalciDisplay(valueInCalciDisplay.toString());
+        valueInCalciDisplay = "";
+        calciMemory = [];
+        console.log("C clicked", valueInCalciDisplay, calciMemory)
+    } else if (inputValue === "BS") {
+        console.log("BS clicked")
+    } else {
+        calciMemory.push(inputValue);
+        console.log("Pressed except C and BS", valueInCalciDisplay, calciMemory);
+        if (inputValue === "=") {
+            console.log("Calculate", valueInCalciDisplay, calciMemory);
+            let calculatedResult;
+            for (let i = 0; i < (calciMemory.length - 1); i++) {
+
+            }
+            console.log("= Operator clicked calculate result and update display with result then set valueInCalciToinitial");
+            calculatedResult = "1000";
+            valueInCalciDisplay = calculatedResult;
+            updateCalciDisplay(valueInCalciDisplay);
+            valueInCalciDisplay = "";
+            calciMemory = [];
+        } else { 
+          if (inputValue === "/" || inputValue === "*" || inputValue === "+" || inputValue === "*/") {
+              console.log("Opeartor clicked do not update display but set valueInCalciDisplay To initial");
+              valueInCalciDisplay = "";
+          } else {
+            valueInCalciDisplay += inputValue;
+            updateCalciDisplay(valueInCalciDisplay);
+          }
+        }
+    }
 
 });
 
